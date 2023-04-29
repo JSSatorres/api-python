92789aa744beac5596622cd852d5edc29be1d0e5
jssatorres <juansataz.dev@gmail.com>
2023-04-29 23:42:26 +0200

    feat main: create app

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"name": "hola fast api"}
+
+@app.get("/url")
+async def root():
+    return {"url": "http/miDick"}
